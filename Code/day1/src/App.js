57ce6a7b8a9b2e60c9e627774cd07b0b9dc56990
raviasipi <asipiravi@gmail.com>
2022-07-30 20:24:31 +0530

    Day 1

---
+++ Code/day1/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import Footer from './Footer';
+import MyComponent from './MyComponent';
+import Navbar from './Navbar';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Navbar />
+      <MyComponent />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
